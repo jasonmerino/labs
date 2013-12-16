99040d8cbed5ee04cae907bac3c36967de048123
Jason Merino <jasonmerino@gmail.com>
2013-12-16 09:59:07 -0800

    Added initial scaffolding for Backbone examples.

---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require('express'),
+	exphbs  = require('express3-handlebars'),
+	getTemplates = require('./getTemplates'),
+	app = express();
+
+/**
+ * Setup Handlebars templating
+ */
+app.set('views', __dirname + '/examples/');
+app.engine('.hbs', exphbs({
+	extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+/**
+ * Setup routes
+ */
+getTemplates(function(templates) {
+
+	/**
+	 * Run through all templates and register routes
+	 */
+	templates.forEach(function(template) {
+		app.get(template.route, function(req, res) {
+			res.render(template.path);
+		});
+	});
+
+	/**
+	 * Have server listen to port
+	 */
+	app.listen(8080);
+
+	/**
+	 * Notify dev
+	 */
+	console.log('Server listening at localhost:8080...');
+});
