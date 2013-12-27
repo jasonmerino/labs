ee36b4f26ba13fd4d1a3c090d6798e65c17b50b1
kuakman <3dimentionar@gmail.com>
2013-12-27 23:33:41 +0000

    Refactored server.js to use the new stretegy to process routes.

--- server.js
+++ server.js
@@ -1,44 +1,64 @@
 var env = require('./config/env'),
+    fs = require('fs'),
+    path = require('path'),
 	util = require('util'),
 	express = require('express'),
+    handlebars = require('handlebars'),
 	exphbs = require('express3-handlebars'),
-	getTemplates = require('./getTemplates'),
-    path = require('path'),
+    Labs = require('./application/util/class'),
+    router = require('./application/router'),
 	app = express();
 
 /**
- * Setup Handlebars templating
- */
-app.set('views', __dirname + '/examples/');
-app.engine('.hbs', exphbs({ extname: '.hbs' }));
-app.set('view engine', '.hbs');
-
-/**
- * Setup static files path
- */
-app.use(express['static'](path.resolve(__dirname + '/public')));
-
-/**
- * Setup routes
- */
-getTemplates(function(templates) {
-
-	/**
-	 * Run through all templates and register routes
-	 */
-	templates.forEach(function(template) {
-		app.get(template.route, function(req, res) {
-			res.render(template.path);
-		});
-	});
-
-	/**
-	 * Have server listen to port
-	 */
-	app.listen(env.port);
-
-	/**
-	 * Notify dev
-	 */
-	console.log(util.format('Server listening at %s:%d ...', env.host, env.port));
+*    Application Configure
+*
+*    1) Run Unit Tests
+*    2) Build Process
+*    3) Initialize Http Server
+**/
+app.configure(function() {
+    
+    app.set('views', __dirname + '/application/view');
+    app.engine('.hbs', exphbs({ extname: '.hbs', handlebars: handlebars }));
+    app.set('view engine', '.hbs');
+   
+    // HandleBars Layouts
+	// FIXME: Research if we can point a reference to the handlebars version that express3-handlebars is using.
+	// I checked the code and it has that in a private scope.
+    require('handlebars-layouts')(handlebars);
+    handlebars.registerPartial('master', fs.readFileSync('application/view/master.hbs', 'utf8'));
+    handlebars.registerPartial('layout', fs.readFileSync('application/view/layout.hbs', 'utf8'));
+    
+    app.use(express.compress());
+    app.use(express.json());
+    app.use(express.urlencoded());
+    app.use(express.methodOverride());
+    app.enable('strict routing');
+    
+    /** Process Routes **/
+    router.configure({ app: app, path: path.resolve(__dirname, 'application/router')});
+    
+    /** Variables added into Handlerbars template engine **/
+    app.locals.debug = env.debug;
+    app.locals.livereload = env.livereload;
+    
+    /** Static Files Express Handler **/
+    app.use(express.static(path.resolve(__dirname, 'public')));
+    
+    app.configure('development', function() {
+        console.log('Running on Development Environment...');
+        app.use(express.logger('dev'));
+        app.use(express.errorHandler({ dumExceptions: true, showStack: true }));
+    });
+    
+    app.configure('production', function() {
+        console.log('Running on Production Environment...');
+        app.use(express.errorHandler());
+    });
+    
+    app.listen(env.port, function() {
+        /** Notify Dev **/
+        console.log(util.format('Server listening at %s:%d ...', env.host, env.port));
+    });
+    
 });
